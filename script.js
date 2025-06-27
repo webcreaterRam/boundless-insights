1fcd503b172440d8cfc6c8e3f7820c255b57134b
ramkumar <rpal92448@gmail.com>
2025-06-27 09:58:11 +0530

    Remove unused js

--- script.js
+++ script.js
@@ -468,14 +468,3 @@ const setupHamburger = () => {
     initializePortfolio();
   }
 });
-
-// Helper to convert hex to rgba
-function hexToRgba(hex, alpha) {
-  hex = hex.replace('#', '');
-  if (hex.length === 3) hex = hex.split('').map(x => x + x).join('');
-  const num = parseInt(hex, 16);
-  const r = (num >> 16) & 255;
-  const g = (num >> 8) & 255;
-  const b = num & 255;
-  return `rgba(${r},${g},${b},${alpha})`;
-}
